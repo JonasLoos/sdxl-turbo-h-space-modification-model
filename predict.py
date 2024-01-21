8a4bbc5a67b1445bb07f7b915c7376fa45bcabb6
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-01-21 17:14:51 +0100

    Aktualisieren von predict.py

--- predict.py
+++ predict.py
@@ -15,21 +15,20 @@ class Predictor(BasePredictor):
             prompt: str = Input(description="Prompt to generate image for"),
             scales: str = Input(description="Comma separated list of direction modification scales"),
             diffusion_steps: int = Input(description="Number of diffusion steps",  default=1, ge=1),
-            seed: int = Input(description="Seed for random number generator, 0 for random", default=0),
+            seed: int = Input(description="Seed for random number generator", default=42),
     ) -> Path:
         """Run a single prediction on the model"""
         # check inputs
         if diffusion_steps != 1:
             raise NotImplementedError("Diffusion steps != 1 is not supported yet")
+        scales = torch.tensor([float(s) for s in scales.split(',')], device='cuda')
         if len(scales) != len(self.directions):
             raise ValueError("Number of scales must match number of directions")
 
         # run prediction
-        seed = seed if seed != 0 else torch.randint(0, 100000, (1,)).item()
-        scales = torch.tensor([float(s) for s in scales.split(',')], device='cuda')
         def hook_fn(module, input, output):
             '''Modify h-space'''
-            return output + (self.directions * scales).sum(dim=0, keepdim=True)
+            return output + (self.directions * scales.reshape((-1,1,1,1)).sum(dim=0, keepdim=True)
         with torch.no_grad(), self.pipe.unet.mid_block.register_forward_hook(hook_fn):
             img = self.pipe(prompt = prompt, num_inference_steps = diffusion_steps, guidance_scale = 0.0, generator = torch.Generator('cuda').manual_seed(seed)).images[0]
         
