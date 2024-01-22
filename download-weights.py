17ed93ed43c3f864877b69c4c04db331b8674a38
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-01-23 00:18:01 +0100

    use download weights script

---
+++ download-weights.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import torch
+import diffusers
+diffusers.AutoPipelineForText2Image.from_pretrained('stabilityai/sdxl-turbo', torch_dtype=torch.float16, variant='fp16', cache_dir='model')
